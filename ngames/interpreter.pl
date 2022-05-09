f59c83650e22fdb329628f90fcaecbb25ac88356
Nieves Montes Gómez <mnm24896@gmail.com>
2022-05-09 18:38:38 +0200

    interpreter.pl docs

--- ngames/interpreter.pl
+++ ngames/interpreter.pl
@@ -1,4 +1,4 @@
-/** general.pl Action Situation Language rule interpreter
+/** <module> Action Situation Language interpreter
 
 This module includes the definitions of common operators and predicates.
 It also includes the predicates to query the rule base according to the
@@ -38,16 +38,18 @@ into a Python-friendly format (mostly as lists).
 :- op(350, yfx, withProb).
 :- discontiguous (withProb)/2.
 
-% GENERAL-PURPOSE PREDICATES
-% To query all rules and in particular to process the consequences of ``simple''
+% # General-purpose predicates
+
+% To query all rules and in particular to process the consequences of simple
 % rules (boundary, position, choice and payoff).
 
-%! query(?Term:term) is det
+%! query(?Term) is det
 %
 % Substitute for the usual call/1 predicate, but includes support for
 % terms expressed as conjunctions (e.g. A and B).
 %
 % @param Term A Prolog term to be queried.
+
 query(A) :-
     call(A).
 
@@ -56,33 +58,28 @@ query(A and B) :-
     query(B).
 
 
-%! query_rule(?Rule:term) is det.
+%! query_rule(?Rule) is det.
 %
 % Return True if Rule is active given the current state of the system.
 %
-% @param Rule A 'rule/4` predicate.
-% query_rule(rule(ID,Type,Priority,if Condition then Consequence
-%         where Constraints)) :-
-%     rule(ID,Type,Priority,if Condition then Consequence where Constraints),
-%     constraint_rule(rule(ID,Type,Priority,if Condition then Consequence where Constraints)),
-%     maplist(query,[Condition|Constraints]).
+% @param Rule A _rule/4_ statement.
 
 query_rule(rule(ID,Type,Priority,if Condition then Consequence where Constraints)) :-
 	rule(ID,Type,Priority,if Condition then Consequence where Constraints),
 	maplist(query,[Condition|Constraints]).
 
 
-%! find_consequences(+ID:atom,+Type:atom,+Threshold:int,-L:list) is det
+%! find_consequences(+ID,+Type,+Threshold,-L) is det
 %
-% Find the instantitations of the if-then-where rules of the 'Type` kind
+% Find the instantitations of the if-then-where rules of the _Type_ kind
 % that are currently active, and extract their consequences paired with their
-% priority. Active rules with priority larger than 'Threshold` are excluded.
+% priority. Active rules with priority larger than _Threshold_ are excluded.
 %
 % @param ID Identifier for the action situation.
-% @param Type One of either 'boundary`, 'position`, 'choice`, 'control` or
-%          'payoff`.
+% @param Type One of either _boundary_, _position_, _choice_ or _control_.
 % @param Threshold The maximum priority of rules to be considered.
 % @param L The output list with the processed consequences.
+
 find_consequences(ID,Type,Threshold,L) :-
     Rule = rule(ID,Type,Priority,if _ then Consequence where _),
     findall(Priority-Consequence,query_rule(Rule),L1),
@@ -91,15 +88,16 @@ find_consequences(ID,Type,Threshold,L) :-
     reverse(L3,L).
 
 
-%! delete_key_gt(L:list,N:int,NewL:list) is det
+%! delete_key_gt(L,N,NewL) is det
 %
 % Auxiliary predicate to delete consequences whose priority is over some
 % given threshold.
 %
 % @param L List with priority-fluents pairs to be processed.
-% @param N Threshold, fluents with priority over it are to be excluded.
-% @param NewL List identical to 'L`, but fluents with priority over Threshold
-%          are excluded.
+% @param N Integer threshold, fluents with priority over it are to be excluded.
+% @param NewL List identical to _L_, but fluents with priority over Threshold
+% are excluded.
+
 delete_key_gt([],_,[]) :- !.
 delete_key_gt([H|T],N,L) :-
     H = Priority-_,
@@ -109,7 +107,7 @@ delete_key_gt([H|T],N,[H|L]) :-
     delete_key_gt(T,N,L).
 
 
-%! process_consequences(+Conseqs:list,+OldParts:list,?NewParts:list) is det
+%! process_consequences(+Conseqs,+OldParts,?NewParts) is det
 %
 % Get all the consequences of some rule type and process them in decreasing
 % order of priority. It returns a new list indicating the consequences that
@@ -123,8 +121,9 @@ delete_key_gt([H|T],N,[H|L]) :-
 %
 % @param Conseqs List of consequence of some rule type.
 % @param OldParts List of old consequences. Intended to be called with the
-%              empty list.
+% empty list.
 % @param NewParts List of the consequences that hold, negations included.
+
 process_consequences([],OldPartList,OldPartList).
 process_consequences(ConseqList,OldPartList,NewPartList) :-
     ConseqList = [_-V1|T],
@@ -135,7 +134,8 @@ add_conseq(C,Old,Old) :- member(C,Old),!.
 add_conseq(C,Old,Old) :- member(~C,Old),!.
 add_conseq(C,Old,[C|Old]).
 
-%! get_simple_consequences(+ID:str,+Type:str,+Threshold:int,-L:list) is det
+
+%! get_simple_consequences(+ID,+Type,+Threshold,-L) is det
 %
 % Process the consequences of boundary, position, choice and payoff rules.
 % It gets the consequences of the rules with the given identification and
@@ -144,56 +144,64 @@ add_conseq(C,Old,[C|Old]).
 % a list of fluents.
 %
 % @param ID Identifier of the action situation.
-% @param Type One of either 'boundary`, 'position`, 'choice` or 'payoff`.
-% @param Threshold Consequences of rules with priorities exceeding `Threshold'
-%               are excluded.
+% @param Type One of either _boundary_, _position_, _choice_ or _payoff_.
+% @param Threshold Consequences of rules with priorities exceeding _Threshold_
+% are excluded.
+
 get_simple_consequences(ID,Type,Threshold,L) :-
     find_consequences(ID,Type,Threshold,L1),
     process_consequences(L1,[],L2),
     delete(L2,~_,L).
 
+
 %%-----------------------------------------------------------------------------
 
-% PROCESSING CONTROL RULES
+% # Processing control rules
+
 % Predicates to find whether a triggered control rule is compatible
 % with the potential next states already established.
 
-%! control_conseq_fact_incompatible(+Fact:atom,+S:list) is det
+%! control_conseq_fact_incompatible(+Fact,+S) is det
 %
 % Check whether a single fact is compatible with a list of established facts.
 %
 % @param Fact The fluent whose compatibility we want to check.
 % @param S List of previously established facts.
+
 control_conseq_fact_incompatible(_,[]) :- !.
 control_conseq_fact_incompatible(F,[S1|S2]) :-
     incompatible(F,S1),
     control_conseq_fact_incompatible(F,S2).
 
-%! control_conseq_incompatible(+Facts:atom,+S:list) is det
+
+%! control_conseq_incompatible(+Facts,+S) is det
 %
-% Checks whether the fluents in 'Facts` that make up a joint consequence
+% Checks whether the fluents in _Facts_ that make up a joint consequence
 % statement of an active control rule are incompatible with the next states
-% already derived in 'S`.
+% already derived in _S_.
 %
 % @param Facts Facts derived from a new control rule. Either a single fact
-%              or a conjunction of them (aka 'A and B`).
+% or a conjunction of them (aka _A and B_).
 % @param S List of potential next state descriptions already derived
-%          (aka a list of lists).
+% (aka a list of lists).
+
 control_conseq_incompatible(F1 and F2,S) :-
     control_conseq_fact_incompatible(F1,S),
     control_conseq_incompatible(F2,S).
 control_conseq_incompatible(F,S) :-
     control_conseq_fact_incompatible(F,S).
 
-%! control_rule_incompatible(+Conseqs:list,+S:list) is det
+
+%! control_rule_incompatible(+Conseqs,+S) is det
 %
-% Check whether the 'Conseqs` list of an active control rule is compatible
-% with the next states already derived in 'S`.
+% Check whether the _Conseqs_ list of an active control rule is compatible
+% with the next states already derived in _S_.
 %
 % @param Conseqs List of facts derived from a control rule. Each of them is
-%                either a single fact or a conjunction (aka 'A and B`).
+% either a single fact or a conjunction (aka _A and B_).
 % @param S List of potential next state descriptions already derived
-%          (aka a list of lists).
+% (aka a list of lists).
+
 control_rule_incompatible([],_).
 control_rule_incompatible([C withProb _|T],S) :-
     control_conseq_incompatible(C,S),
@@ -203,24 +211,25 @@ control_rule_incompatible([C withProb _|T],S) :-
 % Predicates to add the consequences of a single rule into the set of
 % potential next states.
 
-%! add_rule_conseqs_to_next_states(+Conseqs:list,+S:list,+P:list,+OldNextS:list,+OldNextP:list,-NewNextS:list,-NewNextP:list) is det
+%! add_rule_conseqs_to_next_states(+Conseqs,+S,+P,+OldNextS,+OldNextP,-NewNextS,-NewNextP) is det
 %
-% Given the potential next states derived in 'S` and their probabilities
-% in 'P`, add the consequences in 'Conseqs` of an active control rule.
+% Given the potential next states derived in _S_ and their probabilities
+% in _P_, add the consequences in _Conseqs_ of an active control rule.
 %
 % Intended to be called as:
 %
 % ?- add_rule_conseqs_to_next_states(Conseqs,S,P,[],[],NewS,NewP).
 %
 % @param Conseqs A list of consequences from an active control rule,
-%                with the format 'Facts withProb P`.
+% with the format _Facts withProb P_.
 % @param S List of alredy derived next states.
 % @param P List of the probabilities of the already derived next states.
 % @param OldNextStates List of partially processed next states.
-% @param OldNextP List of partially processed next states' probabilities.
-% @param NewNextS List of next states after processing all of 'Conseqs`.
-% @param NewNextP List of next states' probabilities after processing all
-%                 of 'Conseqs`.
+% @param OldNextP List of partially processed next states probabilities.
+% @param NewNextS List of next states after processing all of _Conseqs_.
+% @param NewNextP List of next states probabilities after processing all
+% of _Conseqs_.
+
 add_rule_conseqs_to_next_states([],_,_,OldNextStates,OldNextP,OldNextStates,
         OldNextP).
 add_rule_conseqs_to_next_states([C withProb X|T],S,P,OldNextStates,OldNextP,
@@ -231,117 +240,131 @@ add_rule_conseqs_to_next_states([C withProb X|T],S,P,OldNextStates,OldNextP,
     add_rule_conseqs_to_next_states(T,S,P,IntNextStates,IntNextP,NewNextStates,
     NewNextP).
 
-%! add_joint_conseqs_to_next_states(+F:term,+OldNextStates:list,+OldProb:list,-NewNextStates:list,-NewProb:list) is det
+
+%! add_joint_conseqs_to_next_states(+F,+OldNextStates,+OldProb,-NewNextStates,-NewProb) is det
 %
 % Given a fact (or conjunction of facts) alonside with their probability
-% ('C withProb P`), derived from an activated control rule, update the list of
-% 'OldNextStates` and their probabilities 'OldProb` into 'NewNextStates`
-% and 'NewProb`.
+% (_C withProb P_), derived from an activated control rule, update the list of
+% _OldNextStates_ and their probabilities _OldProb_ into _NewNextStates_
+% and _NewProb_.
 %
 % @param F Consequence derived from an active control rule, joint by
-%          operator 'withProb` to its probability.
+% operator _withProb_ to its probability.
 % @param OldNextStates List of next states prior to update.
-% @param OldProb List of next states' probabilities prior to update.
+% @param OldProb List of next states probabilities prior to update.
 % @param NewNextStates List of updated next states.
-% @param NewProb List of updated next states' probabilities.
+% @param NewProb List of updated next states probabilities.
+
 add_joint_conseqs_to_next_states(_,[],[],[],[]).
 add_joint_conseqs_to_next_states(C withProb X,[OldS1|OldS2],[OldP1|OldP2],
         [NewS1|NewS2],[NewP1|NewP2]) :-
     add_joint_conseqs_to_single_state(C withProb X,OldS1,OldP1,NewS1,NewP1),
     add_joint_conseqs_to_next_states(C withProb X,OldS2,OldP2,NewS2,NewP2).
 
-%! add_joint_conseqs_to_single_state(+F:term,+State:list,+Prob:float,-NewState:list,-NewProb:float) is det
+
+%! add_joint_conseqs_to_single_state(+F,+State,+Prob,-NewState,-NewProb) is det
 %
-% Takes one consequence fact 'F` (expressed as 'C withProb P`)
-% from an activated control rule, a state 'S` to be updated (as a list of
-% fluents) with probability 'Prob` and return the updated state fluents
+% Takes one consequence fact _F_ (expressed as _C withProb P_)
+% from an activated control rule, a state _S_ to be updated (as a list of
+% fluents) with probability _Prob_ and return the updated state fluents
 % and probability.
 %
 % @param F A fact (or conjunction of facts) to append to the partially
-%          derived state 'S`.
+% derived state _S_.
 % @param State List of facts that make up a partially derived state.
 % @param Prob Intermediate probability of the partially derived state.
 % @param NewState List of updated state fluents.
-% @param NewProb Updated probability ('i.e.` product of intermediate state
-%                probability and probability of the derived facts).
+% @param NewProb Updated probability (i.e. product of intermediate state
+% probability and probability of the derived facts).
+
 add_joint_conseqs_to_single_state(C withProb X,State,Prob,NewState,NewProb) :-
     joint_conseqs_to_list([],C,L),
     append(State,L,NewState),
     NewProb is X*Prob.
 
-%! joint_conseqs_to_list(+Old:list,+F:atom,-New:list) is det
+
+%! joint_conseqs_to_list(+Old,+F,-New) is det
 %
 % Auxiliary predicate to append a fact or a conjunction of facts
-% (aka 'A and B`) to a list of fluent.
+% (aka _A and B_) to a list of fluent.
 %
 % @param Old List where new fluents are to be appended.
 % @param F Fact or conjunction of facts to be appended.
 % @param New Updated list of facts.
+
 joint_conseqs_to_list(Old,C1 and C2,New) :-
     !,joint_conseqs_to_list([C1|Old],C2,New).
 joint_conseqs_to_list(Old,C1,[C1|Old]).
 
+
 % Predicates to drag the compatible facts from the pre-transition state
 % to the potential next states.
 
-%! drag_compatible_fact(+PreTranFact:atom,+PostTranState:list,-UpdatedPostTransState:list) is det
+%! drag_compatible_fact(+PreTranFact,+PostTranState,-UpdatedPostTransState) is det
 %
 % If compatible, update a provisional next state with a fluent from the pre-
 % transition state.
 %
 % @param PreTranFact A fluent that holds true in some pre-transition state.
 % @param PostTranState Partially constructed post-transition state.
-% @param UpdatedPostTransState Post transition state updated with 'PreTranFact`.
+% @param UpdatedPostTransState Post transition state updated with _PreTranFact_.
+
 drag_compatible_fact(PreTranFact,NewState,NewState) :-
     incompatible(PreTranFact,NewState),!.
 drag_compatible_fact(PreTranFact,NewState,[PreTranFact|NewState]).
 
-%! drag_compatible_fact(+PreTranFact:list,+PostTranState:list,-UpdatedPostTransState:list) is det
+
+%! drag_compatible_fact(+PreTranFact,+PostTranState,-UpdatedPostTransState) is det
 %
-% Update a partially constructed post-transition state 'PostTranState` with
-% the compatible facts from pre-transition state 'PreTranFact'
+% Update a partially constructed post-transition state _PostTranState_ with
+% the compatible facts from pre-transition state _PreTranFact_
 %
 % @param PreTranFact List of pretransition state facts.
 % @param PostTranState Partially constructed post-transition state, as a list
-%                      of facts.
+% of facts.
 % @param UpdatedPostTransState Updated post-transition state.
+
 drag_compatible_state([],NewState,NewState).
 drag_compatible_state([F|T],NewState,UpdatedNewState) :-
     drag_compatible_fact(F,NewState,Int),
     drag_compatible_state(T,Int,UpdatedNewState).
 
-%! update_all_new_states(+PreTranState:list,+PostTranStates:list,-UpdatedPostTransStates:list) is det
+
+%! update_all_new_states(+PreTranState,+PostTranStates,-UpdatedPostTransStates) is det
 %
-% Update all the potential next states sin 'PostTranStates` with the compatible
-% facts in 'PreTranState`.
+% Update all the potential next states in _PostTranStates_ with the compatible
+% facts in _PreTranState_.
 %
 % @param PreTranState List of fluents in the pre-transition state.
-% @param PostTranStates Partially constructed post-transition states
-%                       (aka a list of lists).
+% @param PostTranStates Partially constructed post-transition states 
+% (aka a list of lists).
 % @param UpdatedPostTransStates Updated post-transition states.
+
 update_all_new_states(_,[],[]).
 update_all_new_states(PreTranState,[S1|S2],[NewS1|NewS2]) :-
     drag_compatible_state(PreTranState,S1,NewS1),
     update_all_new_states(PreTranState,S2,NewS2).
 
+
 % Predicates to find the next state based on the actions performed.
 
-%! add_control_rules(+RuleConseqs:list,+OldNextS:list,+OldNextP:list,-NewNextS:list,-NewNextP:list) is det
+%! add_control_rules(+RuleConseqs,+OldNextS,+OldNextP,-NewNextS,-NewNextP) is det
 %
 % Given the priority-consequences pairs of the activated control rules in
-% 'RuleConseqs`, append them to the provisional states in 'OldNextS`
-% with their unadapted probabilities in 'OldNextP`. If the rule is compatible
-% with the facts already established, add the consequences into 'NewNextS`
-% and update the probabilities into 'NewNextP`.
+% _RuleConseqs_, append them to the provisional states in _OldNextS_
+% with their unadapted probabilities in _OldNextP_. If the rule is compatible
+% with the facts already established, add the consequences into _NewNextS_
+% and update the probabilities into _NewNextP_.
 %
 % @param RuleConseqs List of priority-facts consequences derived from the
-%                    activated control rules.
+% activated control rules.
 % @param OldNextS List of unupdated post-transition states (aka a list of
-%                 lists).
+% lists).
 % @param OldNextP List of unupdated probabilities for the post-transition
-%                 states.
+% states.
 % @param NewNextS List of updated post-transition states (aka a list of lists).
 % @param NewNextP List of updated post-transition states probabilities.
+
 add_control_rules([],OldNextS,OldNextP,OldNextS,OldNextP).
 add_control_rules([_-Conseqs|T],OldNextS,OldNextP,NewNextS,NewNextP) :-
     (control_rule_incompatible(Conseqs,OldNextS) ->
@@ -350,18 +373,20 @@ add_control_rules([_-Conseqs|T],OldNextS,OldNextP,NewNextS,NewNextP) :-
                                         IntNextS,IntNextP),
         add_control_rules(T,IntNextS,IntNextP,NewNextS,NewNextP)).
 
-%! get_control_consequences(+ID:str,+Threshold:int,+PreTranState:list,-PostTranState:list,-Probs:list) is det
+
+%! get_control_consequences(+ID,+Threshold,+PreTranState,-PostTranState,-Probs) is det
 %
 % Gather the consequences of control rules given the current state for the
-% action situation of interest. The rules whose priority exceeds 'Threshold`
+% action situation of interest. The rules whose priority exceeds _Threshold_
 % are excluded.
 %
 % @param ID Identifier of the action situation.
 % @param Threshold Rules whose priority exceed it are excluded from processing.
 % @param PreTranState List of pre-transition state facts.
 % @param PostTranState List of possible post-transition states
-%                      (aka a list of lists).
+% (aka a list of lists).
 % @param Probs List of probabilities of the post-transition states.
+
 get_control_consequences(ID,Threshold,PreTranState,PostTranStates,Probs) :-
     find_consequences(ID,control,Threshold,L1),
     add_control_rules(L1,[[]],[1],L2,Probs),
